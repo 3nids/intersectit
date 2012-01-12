9cd6f97bccc1ac5a36c902caaff42bbd075e31dd
Denis Rouzaud <denis.rouzaud@gmail.com>
2012-01-12 16:23:16 +0100

    started place dimension arc

---
+++ place_arc.py
@@ -0,0 +1,35 @@
+"""
+Triangulation QGIS plugin
+Denis Rouzaud
+denis.rouzaud@gmail.com
+Jan. 2012
+
+Init dialog for distance
+"""
+
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+from ui_place_arc import Ui_placeArc
+
+try:
+    _fromUtf8 = QString.fromUtf8
+except AttributeError:
+    _fromUtf8 = lambda s: s
+
+# create the dialog to connect layers
+class placeArc(QDialog, Ui_placeArc ):
+	def __init__(self,xyrp):
+		QDialog.__init__(self)
+		# Set up the user interface from Designer.
+		self.setupUi(self)
+		QObject.connect(self , SIGNAL( "accepted()" ) , self.addArcToLayer)
+		QObject.connect(self.radiusSpin,   SIGNAL("valueChanged(int)"),	self.radiusSlider, SLOT("setValue(int)"));
+		QObject.connect(self.radiusSlider, SIGNAL("valueChanged(int)"),	self.radiusSpin,   SLOT("setValue(int)"));
+
+		self.settings = QSettings("Triangulation","Triangulation")
+
+		
+
+
+	def addArcToLayer(self):
+		print 1
