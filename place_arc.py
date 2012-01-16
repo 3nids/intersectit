20f585f78b66c9137cb605e2a2d59cf398a1977a
Denis Rouzaud <denis.rouzaud@gmail.com>
2012-01-16 13:24:46 +0100

    place_arc [still not complete]

--- place_arc.py
+++ place_arc.py
@@ -11,6 +11,7 @@ from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 from qgis.core import *
 from qgis.gui import *
+import math
 from ui_place_arc import Ui_placeArc
 
 try:
@@ -32,12 +33,13 @@ class placeArc(QDialog, Ui_placeArc ):
 
 		self.settings = QSettings("Triangulation","Triangulation")
 		
+		radius = 40
 		self.xyrpi = xyrpi
 		self.arc = []
 		for c in xyrpi:
-			self.arc.append(arc(iface,layer,triangulatedPoint,c[0]))
+			point = c[0]
+			self.arc.append(arc(iface,layer,triangulatedPoint,point,radius))
 		
-		self.arc[0].draw(40)
 		
 	def radiusChanged(self,radius):
 		self.arc[0].draw(radius)
@@ -49,10 +51,13 @@ class placeArc(QDialog, Ui_placeArc ):
 		
 		
 class arc():
-	def __init__(self,iface,layer,triangulatedPoint,distancePoint):
+	def __init__(self,iface,layer,triangulatedPoint,distancePoint,radius):
+		self.iface = iface
 		self.layer = layer
 		self.rubber = QgsRubberBand(iface.mapCanvas())
 		self.rubber.setWidth(2)
+		
+		self.length = math.sqrt( triangulatedPoint.sqrDist(distancePoint) )
 		
 		self.triangulatedPoint = triangulatedPoint
 		self.distancePoint     = distancePoint
@@ -60,20 +65,56 @@ class arc():
 		self.direction         = [ -(triangulatedPoint.y()-distancePoint.y())   ,  triangulatedPoint.x()-distancePoint.x()    ]
 		self.way = 1
 		
+		# create feature and geometry
+		f = QgsFeature()
+		f.setGeometry(self.geometry(radius))
+		# look for dimension label
+		dimFieldName = QgsProject.instance().readEntry("Triangulation", "dimension_field", "")[0]
+		ilbl = self.layer.dataProvider().fieldNameIndex(dimFieldName)
+		if ilbl != -1:
+			f.addAttribute(ilbl,"%.2f" % self.length)
+		# look for primary key
+		iid  = self.layer.dataProvider().fieldNameIndex('id')
+		iid = -1
+		if iid != -1:
+			f.addAttribute(iid,'nextval(distribution.dimension_id_seq::regclass)')
+		# add feature to layer	
+		self.layer.dataProvider().addFeatures( [f] )
+		self.layer.updateExtents()
+		self.iface.mapCanvas().refresh()
+		# save id
+		print f.attributeMap()
+		print f.id()
+		self.fid = f.id()
+		
 		
 	def reverse(self):
 		self.way *= 1	
 		
-		
+	
 	def draw(self,radius):
+		f = QgsFeature()
+		self.layer.dataProvider().featureAtId(self.fid,f)
+		f.setGeometry(self.geometry(radius))
+		
+		
+		
+	def geometry(self,radius):
 		# http://www.vb-helper.com/howto_find_quadratic_curve.html
+		"""
 		self.rubber.reset()
 		self.rubber.addGeometry(QgsGeometry.fromPoint(self.triangulatedPoint),self.layer)
 		self.rubber.addGeometry(QgsGeometry.fromPoint(self.distancePoint),self.layer)
-
+		"""
 		curvePoint = QgsPoint(   self.anchorPoint[0] + self.way * self.direction[0] * radius/100    ,   self.anchorPoint[1] + self.way * self.direction[1] * radius/100    )
+		return  QgsGeometry().fromMultiPoint([self.triangulatedPoint,curvePoint,self.distancePoint])  
+
 		
-		self.rubber.addGeometry(QgsGeometry.fromPoint(curvePoint),self.layer)
+		
+				
+		#self.rubber.addGeometry(QgsGeometry.fromPoint(curvePoint),self.layer)
+		
+	
 		
 		
 		
