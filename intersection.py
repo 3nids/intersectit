02aea87d4c774be1855be71cee4f5b022c8c45fc
Denis Rouzaud <denis.rouzaud@gmail.com>
2012-03-16 11:56:00 +0100

    calculation of resdiuals and sigmapos

--- intersection.py
+++ intersection.py
@@ -21,12 +21,12 @@ class intersection:
 	def __init__(self,initPoint,observations):
 		self.initPoint = initPoint
 		self.observations = observations
-		self.nc = len(observations)
+		self.nObs = len(observations)
 		
 	def getSolution(self):
-		if self.nc<2:
+		if self.nObs<2:
 			raise NameError(QApplication.translate("IntersectIt", "Less than 2 observations were found within threshold.", None, QApplication.UnicodeUTF8))
-		elif self.nc==2:
+		elif self.nObs==2:
 			pts = self.twoCirclesIntersect()
 			d1 = pts[0].sqrDist(self.initPoint)
 			d2 = pts[1].sqrDist(self.initPoint)
@@ -41,10 +41,15 @@ class intersection:
 		# distance equation: (x - xc)^2 + (y-yc)^2 - l^2 = 0 (obs: r, param: xc,yc, fixed: x,y)
 		threshold = .0005 # in meters
 		# initial parameters
-		x0  = np.array( [ self.initPoint.x() , self.initPoint.y() ] )
-		print "Initial: %f,%f" % (x0[0],x0[1])
-		dx = [2*threshold,2*threshold]
+		x0  = np.array( [ [self.initPoint.x()] , [self.initPoint.y()] ] ) # brackets needed to create column and not row vector
+		print "Initial position: %f,%f" % (x0[0],x0[1])
+		dx =  np.array( [ [2*threshold],[2*threshold] ] )
+		it = 0
+		# global observations vector
+		l = np.array( [ [obs["measure"]] for obs in self.observations] ) # brackets needed to create column and not row vector
+		# adjustment main loop
 		while max(np.abs(dx))>threshold:
+			it += 1
 			# init matrices
 			A   = []
 			B   = []
@@ -53,37 +58,44 @@ class intersection:
 			for i,obs in enumerate(self.observations):
 				if obs["type"] == "distance":
 					# jacobian for parameters
-					A.append( [2*x0[0]-2*obs["x"] , 2*x0[1]-2*obs["y"]] )
+					A.append( [2*x0[0][0]-2*obs["x"] , 2*x0[1][0]-2*obs["y"]] )
 					# jacobian for observations
 					B.append(-2*obs["measure"])
 					# stochastic model
-					Qll.append( math.pow(obs["precision"],2))
+					Qll.append( math.pow(obs["precision"]/1000,2))
 					# misclosure
-					w.append( math.pow(x0[0]-obs["x"],2) + math.pow(x0[1]-obs["y"],2) - math.pow(obs["measure"],2) )
+					w.append( [ math.pow(x0[0]-obs["x"],2) + math.pow(x0[1]-obs["y"],2) - math.pow(obs["measure"],2) ] ) # brackets needed to create column and not row vector
 					
 			# generate matrices
 			A   = np.array( A   )
 			B   = np.diag(  B   )
 			Qll = np.diag(  Qll )
-			w   = np.array(  w   )
-			
+			w   = np.array(  w  )
+			print A
+						
 			# weight matrix
 			Pm  = np.dot( B , np.dot(Qll,B.T) )
 			P   = la.inv( Pm )
 			# normal matrix
-			N = np.dot( A.T , np.dot(P,A) )
-			u = np.dot( A.T , np.dot(P,w) )
+			N   = np.dot( A.T , np.dot(P,A) )
+			u   = np.dot( A.T , np.dot(P,w) )
 			# QR decomposition
 			q,r = la.qr(N)
 			p   = np.dot(q.T,u)
 			dx  = np.dot(la.inv(r),p)
 			x0 -= dx
-			print "Correction: %f,%f" % (dx[0],dx[1])
-		print "Solution: %f,%f" % (x0[0],x0[1])
+			print "Iteration %u Correction: %f,%f" % (it,dx[0],dx[1])
 		Qxx = la.inv(N)
 		p1 = math.sqrt(Qxx[0][0])
 		p2 = math.sqrt(Qxx[1][1])
-		print p1,p2
+		# residuals -Qll*B'*( P * (A* dx(iN)+w) ) !!! ToBeChecked !!!
+		v = np.dot( -Qll , np.dot( B.T , np.dot( P , np.dot( A,dx) + w ) ) )
+		print "Intersection: %f,%f" % (x0[0],x0[1])
+		print "Precision: %.3f %.3f" % (p1,p2)
+		print "Observations\t\tx\t\ty\tMeasure\tPrecision\tResidual"
+		for i,obs in enumerate(self.observations): print "%s\t%10.3f\t%10.3f\t%.3f\t%.3f\t%f" % (obs["type"],obs["x"],obs["y"],obs["measure"],obs["precision"],1000*v[i][0])
+		sigmapos =  np.dot( v.T , np.dot( P , v ) ) / (self.nObs -2 ) # vTPv / r
+		print "Sigma  a posterior: %f" % sigmapos
 		return QgsPoint(x0[0],x0[1])
 						
 	def twoCirclesIntersect(self):
