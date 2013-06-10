3007595f106cd2c6e9928ca2b3a236b039022161
Denis Rouzaud <denis.rouzaud@gmail.com>
2013-06-10 16:55:37 +0200

    least squares for prolongations

--- core/leastsquares.py
+++ core/leastsquares.py
@@ -27,7 +27,7 @@
 #
 #---------------------------------------------------------------------
 
-import math
+from math import pow, sqrt, sin, cos, pi
 import numpy as np
 from numpy import linalg as la
 
@@ -38,10 +38,12 @@ from mysettings import MySettings
 
 class LeastSquares():
     def __init__(self, observations, initPoint):
+        deg2rad = pi/180
         settings = MySettings()
-        nObs = len(observations)
         threshold = settings.value("intersecLSconvergeThreshold")
-        # initial parameters
+        maxIter = settings.value("intersecLSmaxIter")
+        nObs = len(observations)
+        # initial parameters (position x,y)
         x0 = np.array([[initPoint.x()], [initPoint.y()]])  # brackets needed to create column and not row vector
         report = "Initial position: %13.3f %13.3f\n" % (x0[0], x0[1])
         dx = np.array([[2*threshold], [2*threshold]])
@@ -51,7 +53,7 @@ class LeastSquares():
         # adjustment main loop
         while max(np.abs(dx)) > threshold:
             it += 1
-            if it > settings.value("intersecLSmaxIter"):
+            if it > maxIter:
                 x0 = [None, None]
                 report += "\n!!! Maximum iterations reached (%u)" % (it-1)
                 break
@@ -61,17 +63,34 @@ class LeastSquares():
             Qll = []
             w = []
             for i, obs in enumerate(observations):
+                px = obs["x"]
+                py = obs["y"]
                 if obs["type"] == "distance":
-                    # distance equation: (x - xc)^2 + (y-yc)^2 - l^2 = 0 (obs: r, param: xc,yc, fixed: x,y)
+                    r = obs["observation"]
+                    # distance equation: (xc - px)^2 + (yc - py)^2 - r^2 = 0 (obs: r, param: xc,yc, fixed: px,py)
                     # jacobian for parameters
-                    A.append([2*x0[0][0]-2*obs["x"], 2*x0[1][0]-2*obs["y"]])
+                    A.append([2*x0[0][0]-2*px, 2*x0[1][0]-2*py])
                     # jacobian for observations
-                    B.append(-2*obs["observation"])
+                    B.append(-2*r)
                     # stochastic model
-                    Qll.append(math.pow(obs["precision"]/1000, 2))
+                    Qll.append(pow(obs["precision"]/1000, 2))
                     # misclosure
                     # brackets needed to create column and not row vector
-                    w.append([math.pow(x0[0]-obs["x"], 2) + math.pow(x0[1]-obs["y"], 2) - math.pow(obs["observation"], 2)])
+                    w.append([pow(x0[0][0]-px, 2) + pow(x0[1][0]-py, 2) - pow(r, 2)])
+                if obs["type"] == "prolongation":
+                    az = obs["observation"]
+                    sinaz = sin(az*deg2rad)
+                    cosaz = cos(az*deg2rad)
+                    # equation: (xc-px)/sin(az) - (yc-py)/cos(az) = 0 (obs: az, param: xc,yc, fixed: px,py)
+                    # jacobian for parameters
+                    A.append([1/sinaz, -1/cosaz])
+                    # jacobian for observations
+                    B.append((x0[0][0]-px)*deg2rad*cosaz/pow(sinaz, 2) - (x0[1][0]-py)*-deg2rad*sinaz/pow(cosaz, 2))
+                    # stochastic model
+                    Qll.append(pow(obs["precision"]/1000, 2))
+                    # misclosure
+                    # brackets needed to create column and not row vector
+                    w.append([(x0[0][0]-px)/sinaz - (x0[1][0]-py)/cosaz])
             # generate matrices
             A = np.array(A)
             B = np.diag(B)
@@ -90,9 +109,9 @@ class LeastSquares():
             x0 -= dx
             report += "\nCorrection %u: %10.4f %10.4f" % (it, dx[0], dx[1])
         Qxx = la.inv(N)
-        p1 = math.sqrt(Qxx[0][0])
-        p2 = math.sqrt(Qxx[1][1])
-        # residuals -Qll*B'*(P * (A* dx(iN)+w)) !!! ToBeChecked !!!
+        p1 = sqrt(Qxx[0][0])
+        p2 = sqrt(Qxx[1][1])
+        # residuals -Qll*B'*(P * (A* dx(iN)+w)) !!! ToBeChecked todo !!!
         v = np.dot(-Qll, np.dot(B.T, np.dot(P, np.dot(A, dx) + w)))
         report += "\n"
         report += "\nSolution:\t%13.3f\t%13.3f" % (x0[0], x0[1])
@@ -100,9 +119,12 @@ class LeastSquares():
         report += "\n\n Observation  |       x       |       y       |   Measure   | Precision | Residual"
         report += "  \n              |  [map units]  |  [map units]  | [map units] |  [1/1000] | [1/1000]"
         for i, obs in enumerate(observations):
+            resi = 1000*v[i][0]
+            if obs["type"] == "prolongation":
+                resi *= deg2rad
             report += "\n%13s | %13.3f | %13.3f | %11.3f | %9.1f | %7.1f" % (obs["type"], obs["x"], obs["y"],
                                                                              obs["observation"], obs["precision"],
-                                                                             1000*v[i][0])
+                                                                             resi)
         sigmapos = np.dot(v.T, np.dot(P, v)) / (nObs - 2)  # vTPv / r
         if sigmapos > 1.8:
             sigmapos_comment = "precision is too optimistic"
