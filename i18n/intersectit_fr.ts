35fd585dff52c3f2fa7b3a74fdbdfeeeebd1971d
Denis Rouzaud <denis.rouzaud@gmail.com>
2013-12-12 11:21:48 +0100

    completed french translation

--- i18n/intersectit_fr.ts
+++ i18n/intersectit_fr.ts
@@ -1,51 +1,82 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="2.0" language="fr" sourcelanguage="en">
+<!DOCTYPE TS>
+<TS version="2.0" language="fr" sourcelanguage="en">
 <context>
     <name>IntersectIt</name>
     <message>
-        <location filename="intersectit.py" line="59"/>
+        <location filename="intersectit.py" line="75"/>
         <source>settings</source>
         <translation>paramètres</translation>
     </message>
     <message>
-        <location filename="intersectit.py" line="64"/>
+        <location filename="intersectit.py" line="80"/>
         <source>place distance</source>
         <translation>placer une distance</translation>
     </message>
     <message>
-        <location filename="intersectit.py" line="72"/>
+        <location filename="intersectit.py" line="88"/>
         <source>place orientation</source>
         <translation>place une orientation</translation>
     </message>
     <message>
-        <location filename="intersectit.py" line="83"/>
+        <location filename="intersectit.py" line="99"/>
         <source>simple intersection of 2 objects</source>
         <translation>intersection simple de 2 objets</translation>
     </message>
     <message>
-        <location filename="intersectit.py" line="92"/>
+        <location filename="intersectit.py" line="108"/>
         <source>advanced intersection of 2+ observations</source>
         <translation>intersection avancée avec 2 ou plus observations</translation>
     </message>
     <message>
-        <location filename="intersectit.py" line="103"/>
+        <location filename="intersectit.py" line="119"/>
         <source>edit distance dimension</source>
         <translation>éditer les côtes des distances</translation>
     </message>
     <message>
-        <location filename="intersectit.py" line="112"/>
+        <location filename="intersectit.py" line="128"/>
         <source>edit orientation dimension</source>
         <translation>éditer les côtes des orientations</translation>
     </message>
     <message>
-        <location filename="intersectit.py" line="123"/>
+        <location filename="intersectit.py" line="139"/>
         <source>erase construction features</source>
         <translation>effacer les outils de constructions</translation>
     </message>
     <message>
-        <location filename="intersectit.py" line="130"/>
+        <location filename="intersectit.py" line="146"/>
         <source>help</source>
         <translation>aide</translation>
+    </message>
+    <message>
+        <location filename="advancedintersectionmaptool.py" line="132"/>
+        <source>To place the intersection solution, you must select a layer in the settings.</source>
+        <translation>Pour placer une intersection, vous devez choisir une couche dans les paramètres.</translation>
+    </message>
+    <message>
+        <location filename="advancedintersectionmaptool.py" line="142"/>
+        <source>To save the intersection report, please select a field for it.</source>
+        <translation>Pour enregistrer le rapport de l&apos;intersection, vous devenez sélectionner un champ correspondant.</translation>
+    </message>
+    <message>
+        <location filename="advancedintersectionmaptool.py" line="177"/>
+        <source>To place dimensions, you must define a layer in the settings.</source>
+        <translation>Pour enregirstrer les côtes, vous devenez définir une couche correspondante.</translation>
+    </message>
+    <message>
+        <location filename="advancedintersectionmaptool.py" line="189"/>
+        <source>To save the observation in the layer, please select a field for it.</source>
+        <translation>Pour enregistrer l&apos;observation dans la couche, vous devez sélectionner un champ.</translation>
+    </message>
+    <message>
+        <location filename="advancedintersectionmaptool.py" line="200"/>
+        <source>To save the precision of observation, please select a field for it.</source>
+        <translation>Pour enregistrer la précision de l&apos;observation, vous devez sélectionner un champ.</translation>
+    </message>
+    <message>
+        <location filename="intersectiondialog.py" line="83"/>
+        <source>No intersection can be done with less than 2 observations.</source>
+        <translation>Aucune intersection ne peut être obtenue avec moins de 2 observations.</translation>
     </message>
 </context>
 <context>
@@ -81,14 +112,14 @@
         <translation>Intersect It: :: résultats des moindres carrés</translation>
     </message>
 </context>
-<context encoding="UTF-8">
+<context>
     <name>PlaceOrientation</name>
     <message>
         <location filename="ui_place_orientation.ui" line="14"/>
         <source>Place orientation</source>
         <translation>Placer une orientation</translation>
     </message>
-    <message encoding="UTF-8">
+    <message utf8="true">
         <location filename="ui_place_orientation.ui" line="23"/>
         <source>Precision of prolongation [°]</source>
         <translation>Précision de l&apos;orientation</translation>
@@ -98,7 +129,7 @@
         <source>Length of drawn line</source>
         <translation>Longueur de la ligne déssinée</translation>
     </message>
-    <message encoding="UTF-8">
+    <message utf8="true">
         <location filename="ui_place_orientation.ui" line="118"/>
         <source>Angle [°]</source>
         <translation>Angle [°]</translation>
@@ -117,149 +148,174 @@
         <translation>Général</translation>
     </message>
     <message>
-        <location filename="ui_settings.ui" line="40"/>
+        <location filename="ui_settings.ui" line="53"/>
         <source>Map</source>
         <translation>Carte</translation>
     </message>
     <message>
-        <location filename="ui_settings.ui" line="62"/>
-        <source>highlighting</source>
-        <translation>surbrillance</translation>
-    </message>
-    <message>
-        <location filename="ui_settings.ui" line="79"/>
+        <location filename="ui_settings.ui" line="85"/>
         <source>select objects within</source>
         <translation>Sélection dans un rayon de</translation>
     </message>
     <message>
-        <location filename="ui_settings.ui" line="105"/>
+        <location filename="ui_settings.ui" line="111"/>
         <source>pixels</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="ui_settings.ui" line="118"/>
+        <location filename="ui_settings.ui" line="124"/>
         <source>map units</source>
         <translation>unités de la carte</translation>
     </message>
     <message>
-        <location filename="ui_settings.ui" line="151"/>
+        <location filename="ui_settings.ui" line="193"/>
         <source>Observations</source>
         <translation>Mesures</translation>
     </message>
     <message>
-        <location filename="ui_settings.ui" line="157"/>
+        <location filename="ui_settings.ui" line="199"/>
         <source>Distances</source>
         <translation>Distance</translation>
     </message>
     <message>
-        <location filename="ui_settings.ui" line="163"/>
+        <location filename="ui_settings.ui" line="205"/>
         <source>Snapping</source>
         <translation>Accrochage</translation>
     </message>
     <message>
-        <location filename="ui_settings.ui" line="171"/>
+        <location filename="ui_settings.ui" line="213"/>
         <source>no</source>
         <translation>non</translation>
     </message>
     <message>
-        <location filename="ui_settings.ui" line="176"/>
+        <location filename="ui_settings.ui" line="218"/>
         <source>according to project settings</source>
         <translation>selon les réglages des projets</translation>
     </message>
     <message>
-        <location filename="ui_settings.ui" line="181"/>
+        <location filename="ui_settings.ui" line="223"/>
         <source>on vertex of all layers</source>
         <translation>pour toutes les couches</translation>
     </message>
     <message>
-        <location filename="ui_settings.ui" line="189"/>
+        <location filename="ui_settings.ui" line="231"/>
         <source>Default precision for distances [m]</source>
         <translation>Précision par défaut pour les distances</translation>
     </message>
     <message>
-        <location filename="ui_settings.ui" line="215"/>
+        <location filename="ui_settings.ui" line="257"/>
         <source>Orientations</source>
         <translation>Orientations</translation>
     </message>
-    <message encoding="UTF-8">
-        <location filename="ui_settings.ui" line="224"/>
+    <message utf8="true">
+        <location filename="ui_settings.ui" line="266"/>
         <source>Default precision for orientations [°]</source>
         <translation>Précision par défaut pour les orientations [°]</translation>
     </message>
     <message>
-        <location filename="ui_settings.ui" line="241"/>
+        <location filename="ui_settings.ui" line="283"/>
         <source>Length of drawn line</source>
         <translation>Longueur de la ligne déssinée</translation>
     </message>
     <message>
-        <location filename="ui_settings.ui" line="281"/>
+        <location filename="ui_settings.ui" line="323"/>
         <source>Intersection</source>
         <translation>Intersection</translation>
     </message>
     <message>
-        <location filename="ui_settings.ui" line="307"/>
+        <location filename="ui_settings.ui" line="349"/>
         <source>Advanced intersection</source>
         <translation>Intersection avancée</translation>
     </message>
     <message>
-        <location filename="ui_settings.ui" line="313"/>
+        <location filename="ui_settings.ui" line="355"/>
         <source>write intersection report in field</source>
         <translation>écrire le rapport dans le champ</translation>
     </message>
     <message>
-        <location filename="ui_settings.ui" line="403"/>
+        <location filename="ui_settings.ui" line="445"/>
         <source>write point in layer</source>
         <translation>placer le point dans la couche</translation>
     </message>
     <message>
-        <location filename="ui_settings.ui" line="334"/>
+        <location filename="ui_settings.ui" line="376"/>
         <source>m</source>
         <translation>m</translation>
     </message>
     <message>
-        <location filename="ui_settings.ui" line="341"/>
+        <location filename="ui_settings.ui" line="383"/>
         <source>Least-squares Convergence threshold</source>
         <translation>Seuil de convergence des moindres-carrés</translation>
     </message>
     <message>
-        <location filename="ui_settings.ui" line="351"/>
+        <location filename="ui_settings.ui" line="393"/>
         <source>Maximum iterations</source>
         <translation>Nb. max itérations</translation>
     </message>
     <message>
-        <location filename="ui_settings.ui" line="397"/>
+        <location filename="ui_settings.ui" line="439"/>
         <source>Simple intersection</source>
         <translation>Intersection simple</translation>
     </message>
     <message>
-        <location filename="ui_settings.ui" line="424"/>
+        <location filename="ui_settings.ui" line="466"/>
         <source>Dimensions</source>
         <translation>Côtes</translation>
     </message>
     <message>
-        <location filename="ui_settings.ui" line="436"/>
+        <location filename="ui_settings.ui" line="566"/>
         <source>place precision in</source>
         <translation>écrire la précision dans le champ</translation>
     </message>
     <message>
-        <location filename="ui_settings.ui" line="450"/>
+        <location filename="ui_settings.ui" line="556"/>
         <source>place observation value in</source>
         <translation>écrire la valeur mesurée dans</translation>
     </message>
     <message>
-        <location filename="ui_settings.ui" line="476"/>
-        <source>place dimensions in layer</source>
-        <translation>placer les côtes dans la couche</translation>
+        <location filename="ui_settings.ui" line="141"/>
+        <source>None</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="ui_settings.ui" line="499"/>
-        <source>place observation type in</source>
-        <translation>écrire le type d&apos;obervation dans le champ</translation>
+        <location filename="ui_settings.ui" line="146"/>
+        <source>+</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="ui_settings.ui" line="513"/>
-        <source>Check type when editing</source>
-        <translation>Contrôler le type d&apos;observation lors de l&apos;édition</translation>
+        <location filename="ui_settings.ui" line="151"/>
+        <source>X</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message utf8="true">
+        <location filename="ui_settings.ui" line="156"/>
+        <source>□</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_settings.ui" line="161"/>
+        <source>O</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_settings.ui" line="179"/>
+        <source>rubber band size and color</source>
+        <translation>Taille et couleur de la surbrillance</translation>
+    </message>
+    <message>
+        <location filename="ui_settings.ui" line="485"/>
+        <source>place orientation dimensions</source>
+        <translation>placer les côtes pour les orientations</translation>
+    </message>
+    <message>
+        <location filename="ui_settings.ui" line="543"/>
+        <source>in layer</source>
+        <translation>dans la couche</translation>
+    </message>
+    <message>
+        <location filename="ui_settings.ui" line="534"/>
+        <source>place distance dimensions</source>
+        <translation>placer les côtes pour les distance</translation>
     </message>
 </context>
 <context>
